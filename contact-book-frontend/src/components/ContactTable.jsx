7c7d33620e2b1cf5e8c51450bb6af67be275fe0a
Soufiane Amajat <soufianeamt11@gmail.com>
2025-06-18 17:17:15 +0100

    feat : create ContactTable component and setup the head

---
+++ contact-book-frontend/src/components/ContactTable.jsx
@@ -0,0 +1,16 @@
+
+export default function ContactTable ()
+{
+    return (
+        <div>
+            <table>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Phone Number</th>
+                <th>Address</th>
+                <th>Actions</th>
+            </table>
+        </div>
+    )
+}
